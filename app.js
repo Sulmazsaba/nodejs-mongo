2d3a6583ac79d6158e2ad9fababb4620487f5fcf
Sulmaz <sulmazsaba@gmail.com>
2021-05-03 11:12:30 +0430

    add post api

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const db = mongoose.connect("mongodb://localhost/bookAPI");
 // mongo bookAPI < booksJson.js (for creating dab in mongo from bookJson.js file)
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
 
-bookRouter.route("/books").get((req, res) => {
-  const { query } = req;
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(books);
-  });
-});
+app.use(bodyParser.urlencoded({ estended: true }));
+app.use(bodyParser.json());
+
 
 app.use("/api", bookRouter);
 
